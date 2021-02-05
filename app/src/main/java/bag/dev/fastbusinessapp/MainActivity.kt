70e9589bee814d86f10f1068d2dfa79056ecd256
bag911 <bagish3000@gmail.com>
2021-02-06 01:09:40 +0300

    reviewed MainActivity.kt

--- app/src/main/java/bag/dev/fastbusinessapp/MainActivity.kt
+++ app/src/main/java/bag/dev/fastbusinessapp/MainActivity.kt
@@ -2,18 +2,14 @@ package bag.dev.fastbusinessapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.AbsListView
-import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import bag.dev.fastbusinessapp.databinding.ActivityMainBinding
-import java.util.*
 import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var aList = ArrayList<AimList>()
-    lateinit var adapter:ItemAdapter
+    private lateinit var adapter:ItemAdapter
     private val fullTime = 86400
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,12 +25,6 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerV.layoutManager= LinearLayoutManager(this)
         binding.recyclerV.adapter =  adapter
 
-//        binding.progressBar2.progress
-        var isRun = false
-
-        var hours:Int
-        var minutes:Int
-        var seconds:Int
 
         binding.button.setOnClickListener {
             aList.add(AimList(binding.editText.text.toString(),fullTime))
